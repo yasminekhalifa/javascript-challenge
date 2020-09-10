1e04e3f5b034b4437bfbe7a9488d5ffaf29992bb
yasminekhalifa <yasminaekhalifa@gmail.com>
2020-09-10 19:05:41 +1000

    link filter button

--- Ufo-level-1/static/js/app.js
+++ Ufo-level-1/static/js/app.js
@@ -1,10 +1,8 @@
 // from data.js
 var tableData = data;
 var tbody = d3.select("tbody");
-// YOUR CODE HERE!
 
-// Step 5: Use d3 to update each cell's text with
-// weather report values (weekday, date, high, low)
+
 data.forEach((ufo) => {
     var row = tbody.append("tr");
     Object.entries(ufo).forEach(([key, value]) => {
@@ -12,3 +10,33 @@ data.forEach((ufo) => {
       cell.text(value);
     });
 });
+var filter = d3.select("#filter-btn");
+filter.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+    d3.event.preventDefault();
+    var input = d3.select("#datetime");
+    var inputValue = input.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    };
