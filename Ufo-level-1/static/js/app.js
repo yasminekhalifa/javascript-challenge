118287bb50c6671300ede3b4bb8d3c34da78dde6
yasminekhalifa <yasminaekhalifa@gmail.com>
2020-09-12 18:19:35 +1000

    filtertable

--- Ufo-level-1/static/js/app.js
+++ Ufo-level-1/static/js/app.js
@@ -26,17 +26,12 @@ function runEnter() {
     var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
 
     console.log(filteredData);
-    table = document.getElementById("ufo-table");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-          txtValue = td.textContent || td.innerText;
-          if (txtValue.indexOf(filter) > -1) {
-            tr[i].style.display = "";
-          } else {
-            tr[i].style.display = "none";
-          }
-        }
-      }
-    };
+    tbody.html("");
+    filteredData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+};
