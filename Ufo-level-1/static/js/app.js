be51efef5f88a8abed6c9745a49bad9ca869bdf4
yasminekhalifa <yasminaekhalifa@gmail.com>
2020-09-12 18:23:39 +1000

    Added comments

--- Ufo-level-1/static/js/app.js
+++ Ufo-level-1/static/js/app.js
@@ -1,8 +1,7 @@
 // from data.js
 var tableData = data;
 var tbody = d3.select("tbody");
-
-
+// Add data to table 
 data.forEach((ufo) => {
     var row = tbody.append("tr");
     Object.entries(ufo).forEach(([key, value]) => {
@@ -10,23 +9,23 @@ data.forEach((ufo) => {
       cell.text(value);
     });
 });
+// Target filter button 
 var filter = d3.select("#filter-btn");
 filter.on("click", runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
 
-  // Prevent the page from refreshing
+// Prevent the page from refreshing
     d3.event.preventDefault();
+// Set input value
     var input = d3.select("#datetime");
     var inputValue = input.property("value");
-
-    console.log(inputValue);
-
+// Filter data using input value
     var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-
-    console.log(filteredData);
+// Clear table contents
     tbody.html("");
+// Populate table with filtered data 
     filteredData.forEach((ufo) => {
         var row = tbody.append("tr");
         Object.entries(ufo).forEach(([key, value]) => {
